28e24927c2ec466b7d9594e14b32806ed42ab83c
Shreyash Mali <malishreyash2005@gmail.com>
2026-08-16 16:13:25 +0530

    created board

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-const board = document.querySelector('.board');
+const board = document.querySelector('.board');//here we add board class 
+
+//Set the size of each block
 const blockHeight = 30
 const blockWidth = 30
 
-
+//Calculate how many columns can fit
 const col=Math.floor(board.clientWidth/blockWidth);
+//Calculate how many rows can fit
 const row=Math.floor(board.clientHeight/blockHeight);
 
+//Create blocks using a loop 
 for(let i=0;i<row * col;i++){
-    const block = document.createElement('div');
-    block.classList.add('block');
-    board.appendChild(block);
+    const block = document.createElement('div');//Create a new div Every time the loop runs, it creates:<div></div>
+    block.classList.add('block');//Add the block class:<div class="block"></div>
+    board.appendChild(block);//Add the block to the board
 }
